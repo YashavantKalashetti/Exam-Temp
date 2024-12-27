1791f4b043884747800bc81154f16e0369f02b5b
Yashavant <yashavantkalashetti44@gmail.com>
2024-12-28 03:11:35 +0530

    13

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io.connect("https://exam-temp-backend.onrender.com");
+const socket = io.connect("https://exam-backend-demo.onrender.com");
 
 function App() {
   const [me, setMe] = useState("");
